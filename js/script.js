d743a95d7f308655a01009267370ef938c40c8d4
Oan Najmi Zertho <81005679+OanNazer@users.noreply.github.com>
2022-01-08 10:45:19 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
 const menu = document.querySelector('nav');
 const menuBtn = document.querySelector('#menu-btn');
 
+
 menuBtn.addEventListener('click', function() {
   menu.classList.add('transform');
+
 });
+
+var links = document.querySelectorAll('.link');
 
 $('.link').on('click', function(e){
   if(this.hash !== ''){
     e.preventDefault();
     
     menu.classList.remove('transform');
+    links.forEach(function(el){
+      el.classList.remove('active');
+      e.target.classList.add('active');
+    })
     
     const link = this.hash;
     
     $('html, body').animate({
-      scrollTop: $(link).offset().top - 70
+      scrollTop: $(link).offset().top - 50
     }, 1000, 'swing');
   }
   
