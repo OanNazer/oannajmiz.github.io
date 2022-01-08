7d84fecd2b71d8b2cb30d1ae6adf03e615bdef9e
Oan Najmi Zertho <81005679+OanNazer@users.noreply.github.com>
2022-01-08 08:23:09 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,36 @@
 const menu = document.querySelector('nav');
 const menuBtn = document.querySelector('#menu-btn');
 const closeMenu = document.querySelector('#close-menu');
-const closeBtn = document.querySelector('.close-menu');
-const contactBtn = document.querySelector('#contactBtn');
+//const closeBtn = document.querySelector('.close-menu');
+const contactBtn = document.querySelector('.contactBtn');
 const contact = document.querySelector('#contact');
 
-menuBtn.addEventListener('click', function(){
+menuBtn.addEventListener('click', function() {
   menu.classList.add('transform');
-  closeBtn.classList.add('show');
+  //closeBtn.classList.add('show');
 });
 
-closeMenu.addEventListener('click', function(){
+closeMenu.addEventListener('click', function() {
   menu.classList.remove('transform');
-  closeBtn.classList.remove('show');
+  //closeBtn.classList.remove('show');
 });
 
-contactBtn.addEventListener('click', function(){
+contactBtn.addEventListener('click', function() {
   contact.classList.toggle('show');
+});
+
+
+$('.link').on('click', function(e){
+  if(this.hash !== ''){
+    e.preventDefault();
+    
+    menu.classList.remove('transform');
+    
+    const link = this.hash;
+    
+    $('html, body').animate({
+      scrollTop: $(link).offset().top - 70
+    }, 1000, 'swing');
+  }
+  
 });
