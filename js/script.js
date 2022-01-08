1651e36c1a4126d85bd00f36f1d4829e6d408d85
Oan Najmi Zertho <81005679+OanNazer@users.noreply.github.com>
2022-01-08 10:29:19 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,9 @@
 const menu = document.querySelector('nav');
 const menuBtn = document.querySelector('#menu-btn');
-const closeMenu = document.querySelector('#close-menu');
-//const closeBtn = document.querySelector('.close-menu');
-const contactBtn = document.querySelector('.contactBtn');
-const contact = document.querySelector('#contact');
 
 menuBtn.addEventListener('click', function() {
   menu.classList.add('transform');
-  //closeBtn.classList.add('show');
 });
-
-closeMenu.addEventListener('click', function() {
-  menu.classList.remove('transform');
-  //closeBtn.classList.remove('show');
-});
-
-contactBtn.addEventListener('click', function() {
-  contact.classList.toggle('show');
-});
-
 
 $('.link').on('click', function(e){
   if(this.hash !== ''){
