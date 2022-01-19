747c2765024d713546a45376e67ac06f5c0f4430
Oan Najmi Zertho <81005679+OanNazer@users.noreply.github.com>
2022-01-19 10:58:05 +0700

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,65 @@
 var links = document.querySelectorAll('.link');
 
-$('.link').on('click', function(e){
-  if(this.hash !== ''){
+$('.link').on('click', function(e) {
+  if (this.hash !== '') {
     e.preventDefault();
     //menu.classList.remove('transform');
-    links.forEach(function(el){
+    links.forEach(function(el) {
       el.classList.remove('active');
       e.target.classList.add('active');
     })
-    
+
     const link = this.hash;
     console.log(link)
     $('html, body').animate({
       scrollTop: $(link).offset().top - 50
     }, 1000, 'easeInOutExpo');
   }
-  
+
 });
+
+if (window.innerWidth <= 5600) {
+  $(window).scroll(function() {
+    var scroll = $(this).scrollTop();
+
+    // skill box
+    if (scroll > $('#skill').offset().top - 200) {
+      $('#skill .skill-box').each(function(i) {
+        setTimeout(() => {
+          if (scroll > $('.skill-box').eq(i).offset().top - 500) {
+            $('.skill-box').eq(i).addClass('muncul');
+            $('.skill-box .progress-box').each(function(j){
+              console.log($('.progress-box'));
+
+              /*
+              setTimeout(()=>{
+                if(scroll > $('.progress-box').eq(j).offset().top - 300){
+                  
+                  $('.progress-box .progress').eq(j).style.width = $('.skill-box .progress-box .progress-num').textContent;
+                }
+              }, 300*(j+1));
+              */
+
+            });
+          }
+        }, 500 * (i + 1));
+
+      });
+    } else {
+      $('.skill-box').removeClass('muncul');
+    }
+
+    // project box
+  });
+}
+
+/*
+$all('.skill-box').forEach(() => {
+  $all('.progress').forEach((el) => {
+    console.log($('.progress').width($('.progress-num').textContent))
+  });
+});
+*/
+
+const profile = document.querySelector('.profile').getAttribute('alt');
+
